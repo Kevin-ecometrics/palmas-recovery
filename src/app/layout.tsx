a9dc7e1a4ab26ffc8684ce229070551f3195415a
¨kevin180317¨ <ortega1gv@gmail.com>
2026-03-05 16:46:54 -0800

    Update dependencies and enhance layout with Google Analytics and Facebook Pixel integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
+import Script from "next/script";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import I18nProvider from "../i18n/I18nProvider";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
 });
+
+const META_PIXEL_ID = "743057892204187";
 
 export default function RootLayout({
   children,
@@ -16,8 +20,35 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/fav.ico" type="image/x-icon" />
+        {META_PIXEL_ID && (
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');`}
+          </Script>
+        )}
       </head>
       <body className={`${montserrat.variable} antialiased`}>
+        {META_PIXEL_ID && (
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        )}
+        <GoogleTagManager gtmId="GTM-MN5745N8" />
+        <GoogleAnalytics gaId="G-NG4JKZ5NRL" />
         <I18nProvider>{children}</I18nProvider>
       </body>
     </html>
