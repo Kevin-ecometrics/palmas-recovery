ffd0a89c3778570770cd45f388a4602fe703832d
¨kevin180317¨ <ortega1gv@gmail.com>
2026-05-01 17:36:22 -0700

    update images 360

--- src/app/components/data/360.data.ts
+++ src/app/components/data/360.data.ts
@@ -1,81 +1,81 @@
 
   export const sharedRoomImages = [
-    { src: "/360/Habitacion doble bano compartido.jpg", key: "room" },
-    { src: "/360/Primer bano compartido.jpg", key: "bathroom" },
-    { src: "/360/segundo bano compartido.jpg", key: "bathroom" },
+    { src: "/360/Habitacion doble bano compartido.webp", key: "room" },
+    { src: "/360/Primer bano compartido.webp", key: "bathroom" },
+    { src: "/360/segundo bano compartido.webp", key: "bathroom" },
   ];
-  
+
   export const privateRoomImages = [
-    { src: "/360/Habitación sencilla bano compartido.jpg", key: "room" },
+    { src: "/360/Habitación sencilla bano compartido.webp", key: "room" },
   ];
-  
+
   export const largePrivateRoomImages = [
-    { src: "/360/tercer habitacion sencilla con bano compartido.jpg", key: "room" },
+    { src: "/360/tercer habitacion sencilla con bano compartido.webp", key: "room" },
   ];
 
   export const vipRoomImages = [
-    { src: "/360/Habitacion vip con bano propio.jpg", key: "room" },
-    { src: "/360/habitacion vip con bano propio vista 2.jpg", key: "room" },
+    { src: "/360/Habitacion vip con bano propio.webp", key: "room" },
+    { src: "/360/habitacion vip con bano propio vista 2.webp", key: "room" },
   ];
 
   export const lobbyRoomImages = [
-    { src: "/360/2da entrada con elevador.jpg", key: "entrance" },
-    { src: "/360/area comun detallada 2.jpg", key: "commonArea" },
-    { src: "/360/area comun detallada.jpg", key: "commonArea" },
-    { src: "/360/entrada de banos y espejo.jpg", key: "bathroom" },
-    { src: "/360/Entrada por escalera pasillo 1.jpg", key: "hallway" },
-    { src: "/360/Entrada por escaleras.jpg", key: "entrance" },
+    { src: "/360/2da entrada con elevador.webp", key: "entrance" },
+    { src: "/360/area comun detallada 2.webp", key: "commonArea" },
+    { src: "/360/area comun detallada.webp", key: "commonArea" },
+    { src: "/360/entrada de banos y espejo.webp", key: "bathroom" },
+    { src: "/360/Entrada por escalera pasillo 1.webp", key: "hallway" },
+    { src: "/360/Entrada por escaleras.webp", key: "entrance" },
     {
-      src: "/360/Espacio comun con vista a estacion de enfermeras y pasillo de escaleras.jpg",
+      src: "/360/Espacio comun con vista a estacion de enfermeras y pasillo de escaleras.webp",
       key: "commonArea",
     },
     {
-      src: "/360/estacion de enfermeras parte del area comun y pasillo.jpg",
+      src: "/360/estacion de enfermeras parte del area comun y pasillo.webp",
       key: "nurseStation",
     },
     {
-      src: "/360/estacion de enfermeras parte del cuarto vip y parte del area comun.jpg",
+      src: "/360/estacion de enfermeras parte del cuarto vip y parte del area comun.webp",
       key: "nurseStation",
     },
     {
-      src: "/360/Interior de pasillo puertas cerradas pasillo.jpg",
+      src: "/360/Interior de pasillo puertas cerradas pasillo.webp",
       key: "hallway",
     },
     {
-      src: "/360/mismo pasillo de la primera entrada de escaleras.jpg",
+      src: "/360/mismo pasillo de la primera entrada de escaleras.webp",
       key: "hallway",
     },
     {
-      src: "/360/pasillo con primeras dos habitaciones a los lados.jpg",
+      src: "/360/pasillo con primeras dos habitaciones a los lados.webp",
       key: "hallway",
     },
     {
-      src: "/360/pasillo con segundas habitaciones a los lados.jpg",
+      src: "/360/pasillo con segundas habitaciones a los lados.webp",
       key: "hallway",
     },
     {
-      src: "/360/pasillo con vista a las escaleras de cocina y banos.jpg",
+      src: "/360/pasillo con vista a las escaleras de cocina y banos.webp",
       key: "hallway",
     },
     {
-      src: "/360/pasillo donde se ve estacion de enfermeras y cuarto compartido con bano compartido.jpg",
+      src: "/360/pasillo donde se ve estacion de enfermeras y cuarto compartido con bano compartido.webp",
       key: "nurseStation",
     },
     {
-      src: "/360/pasillo que da vista ala estacion de enfermeras y oficina de la dr cecilia.jpg",
+      src: "/360/pasillo que da vista ala estacion de enfermeras y oficina de la dr cecilia.webp",
       key: "nurseStation",
     },
-    { src: "/360/Vista de pasillo cuando vienes de escaleras.jpg", key: "hallway" },
+    { src: "/360/Vista de pasillo cuando vienes de escaleras.webp", key: "hallway" },
     {
-      src: "/360/vista de pasillo lado  de donde vienes de las escaleras.jpg",
+      src: "/360/vista de pasillo lado  de donde vienes de las escaleras.webp",
       key: "hallway",
     },
     {
-      src: "/360/vista mas adentro de pasillo de la entrada de las escaleras.jpg",
+      src: "/360/vista mas adentro de pasillo de la entrada de las escaleras.webp",
       key: "hallway",
     },
     {
-      src: "/360/vista pasillo principal con cuartos  y se ve la  sala de pasillos escaleras de cocina y rampa de banos.jpg",
+      src: "/360/vista pasillo principal con cuartos  y se ve la  sala de pasillos escaleras de cocina y rampa de banos.webp",
       key: "hallway",
     },
   ];
